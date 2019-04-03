76708990c6124a6f10ee5b30b3c2405d56b09f34
yiliang <yiliang@pensees.ai>
2019-04-03 11:13:28 +0800

    Dubbo Demo代码

---
+++ DubboSpringDemoConsumer/src/main/java/com/yiworld/DubboSpringBootDemoConsumer/DubboSpringBootDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.yiworld.DubboSpringBootDemoConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboSpringBootDemoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboSpringBootDemoConsumerApplication.class, args);
+    }
+
+}
---
+++ DubboSpringDemoProvider/src/main/java/com/yiworld/DubboSpringDemoProvider/DubboSpringDemoProviderApplication.java
@@ -0,0 +1,13 @@
+package com.yiworld.DubboSpringDemoProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboSpringDemoProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboSpringDemoProviderApplication.class, args);
+    }
+
+}
