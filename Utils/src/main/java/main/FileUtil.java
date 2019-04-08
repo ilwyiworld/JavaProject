8159f0c6a3608ea98b593bff115f5bfa4b370790
yiliang <yiliang@pensees.ai>
2019-04-08 09:32:26 +0800

    添加PNG转字节码再转回图片工具方法

--- Utils/src/main/java/main/FileUtil.java
+++ Utils/src/main/java/main/FileUtil.java
@@ -1,8 +1,13 @@
 package main;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +16,7 @@ import java.util.List;
  */
 public class FileUtil {
     private List<String> picPaths=new ArrayList<>();
+    Long total=0L;
     public static String writeFile2Str(String filePath) throws Exception {
         File file = new File(filePath);//定义一个file对象，用来初始化FileReader
         FileReader reader = new FileReader(file);//定义一个fileReader对象，用来初始化BufferedReader
@@ -45,4 +51,55 @@ public class FileUtil {
             System.out.println("文件不存在!");
         }
     }
+
+    //统计文件数量
+    private File generateFileList() {
+        File fileListFile = new File("C:\\Users\\yiliang\\Desktop\\file.list");
+        // 一次写入文件列表的文件个数
+        int WRITE_TO_FILE_LIST_COUNT_ONCE = 10;
+        File workingDir = new File("C:\\Users\\yiliang\\Desktop\\yi");
+        List<String> filePathNames = new ArrayList<>(WRITE_TO_FILE_LIST_COUNT_ONCE);
+        try {
+            Files.walk(workingDir.toPath(), 5)
+                    .map(path -> path.toFile())
+                    .filter(file->matchedPhotoFile(file))
+                    .forEach(file -> {
+                        filePathNames.add(file.getPath());
+                        if (filePathNames.size() >= WRITE_TO_FILE_LIST_COUNT_ONCE) {
+                            //将文件夹里的文件名按行写入文件中
+                            writeFilePathNamesToListFile(fileListFile, filePathNames);
+                            filePathNames.clear();
+                        }
+                        total++;
+                    });
+            if (filePathNames.size() > 0) {
+                writeFilePathNamesToListFile(fileListFile, filePathNames);
+                filePathNames.clear();
+            }
+            System.out.println(total);
+            return fileListFile;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean matchedPhotoFile(File file) {
+        if (!file.exists() || file.isDirectory()) {
+            return false;
+        }
+        return true;
+    }
+
+    private void writeFilePathNamesToListFile(File fileListFile, List<String> filePathNames) {
+        try {
+            FileUtils.writeLines(fileListFile, filePathNames, true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new FileUtil().generateFileList();
+    }
+
 }
